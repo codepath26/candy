46067e8cbd50f97c8f0c46457444e1a0e1cc97d4
parth thakor <parththakor2610@gmail.com>
2023-08-02 16:34:42 +0530

    Complete Implimentation

--- logic.js
+++ logic.js
@@ -1,11 +1,14 @@
-// getting the data from dom
+
 let ids = [];
+
 let candy = document.getElementById("candy-name");
 let flavor = document.getElementById("flavor");
 let price = document.getElementById("price");
 let quantity = document.getElementById("quantity");
 let form = document.getElementById("add-item");
 let items = document.getElementById("items");
+let alert1 = document.getElementById("alert")
+
 
 // adding the eventlistners
 form.addEventListener("submit", addCandy);
@@ -16,11 +19,16 @@ window.addEventListener("DOMContentLoaded", gettingCandyDetails);
 function gettingCandyDetails() {
   axios
     .get(
-      "https://crudcrud.com/api/eee789f4d71f444bafc280a5366d4dd7/candiesDetails"
+      "https://crudcrud.com/api/33d831562fea40a297815c4234f2823a/candiesDetails"
     )
     .then((response) => {
-      response.data.forEach((candyObj) => {
+      response.data.forEach((candyObj,index) => {
         ids.push(candyObj._id);
+        // if(candyObj.Quantity == 0){
+        //   ids.splice(index,1);
+        //   axios.delete(`https://crudcrud.com/api/33d831562fea40a297815c4234f2823a/candiesDetails/${candyObj._id}`).then(()=>{
+        //   })
+        // }
         displayCandies(candyObj);
       });
       console.log(ids);
@@ -28,19 +36,27 @@ function gettingCandyDetails() {
 }
 
 // adding the candy to the container with quantity
-function addCandy(e) {
+async function addCandy(e) {
   e.preventDefault();
   // console.log(candy.value)
-  let obj = {
-    Candy: candy.value,
+  if(quantity.value == 0 || price.value == 0){
+    alert1.classList.remove("d-none")
+    alert1.classList.add("d-block")
+    setTimeout(() => {
+      alert1.remove();
+    }, 2000);
+  }else{
+
+    let obj = {
+      Candy: candy.value,
     Flavor: flavor.value,
     Price: price.value,
     Quantity: quantity.value,
   };
-  axios
-    .post(
-      "https://crudcrud.com/api/eee789f4d71f444bafc280a5366d4dd7/candiesDetails",
-      obj
+  await axios
+  .post(
+    "https://crudcrud.com/api/33d831562fea40a297815c4234f2823a/candiesDetails",
+    obj
     )
     .then((response) => {
       displayCandies(obj);
@@ -52,6 +68,7 @@ function addCandy(e) {
       price.value = "";
       quantity.value = "";
     });
+  }
 }
 
 // display the data of the candies
@@ -59,82 +76,79 @@ function addCandy(e) {
 function displayCandies(obj) {
   let item = document.createElement("li");
   item.classList.add("list-group-item");
-  // item.appendChild(
-  //   document.createTextNode(
 
-  //     `Candy : ${obj.Candy}, Flavor : ${obj.Flavor}, Price : ${obj.Price} Quantity : ${obj.Quantity}`
-  //   )
-  // );
-  // let buy3 = document.createElement("button");
-  // buy3.classList = "btn btn-success btn-sm float-end buy-3 mx-1";
-  // buy3.appendChild(document.createTextNode("Buy-3"));
-  // let buy1 = document.createElement("button");
-  // buy1.classList = "btn btn-success btn-sm float-end buy-1 mx-1";
-  // buy1.appendChild(document.createTextNode("Buy-1"));
-  // let buy2 = document.createElement("button");
-  // buy2.classList = "btn btn-success btn-sm float-end buy-2 mx-1";
-  // buy2.appendChild(document.createTextNode("Buy-2"));
-  // item.appendChild(buy3);
-  // item.appendChild(buy2);
-  // item.appendChild(buy1);
-  // items.appendChild(item);
-  item.classList.add("list-group-item");
-  items.innerHTML += `<li><span> Candy : ${obj.Candy}, </span><span>  Flavor : ${obj.Flavor},</span> <span>  Price : ${obj.Price},</span> <span class="Quantity">  Quantity : ${obj.Quantity}</span><button class="btn btn-success btn-sm float-end buy-3 mx-1">Buy-3</button><button class="btn btn-success btn-sm float-end buy-2 mx-1">Buy-2</button><button class="btn btn-success btn-sm float-end buy-1 mx-1">Buy-1</button></li>`;
+  items.innerHTML += `<li class ="list-group-item mt-3"><span> Candy : ${obj.Candy}, </span><span>  Flavor : ${obj.Flavor},</span> <span>  Price : ${obj.Price},</span> <span class="Quantity">  Quantity : ${obj.Quantity}</span><button class="btn btn-success btn-sm float-end buy-3 mx-1">Buy-3</button><button class="btn btn-success btn-sm float-end buy-2 mx-1">Buy-2</button><button class="btn btn-success btn-sm float-end buy-1 mx-1">Buy-1</button></li>`;
 }
 
 // now impliment the but button
 function buySomthing(e) {
   if (e.target.classList.contains("buy-1")) {
-    let li = e.target.parentNode;
-    let index = Array.from(li.parentNode.children).indexOf(li);
-    console.log(index);
-    let id = ids[index];
-    axios
-      .get(
-        `https://crudcrud.com/api/eee789f4d71f444bafc280a5366d4dd7/candiesDetails/${id}`
-      )
-      .then((res) => {
-        console.log(res.data);
-        let obj = res.data;
-        //  return new Promise((resolve ,reject)=>{
-        if (obj.Quantity != 0) {
-          obj.Quantity = obj.Quantity - 1;
-          console.log(obj.Quantity);
-          let obj1 = {
-            Candy: obj.Candy,
-            Flavor: obj.Flavor,
-            Price: obj.Price,
-            Quantity: obj.Quantity,
-          };
-          return obj1;
-        } else {
-          // what happen if i return the error and trow the error at here
-          throw new Error("Candies are finished");
-        }
-      })
-      .then((obj) => {
-        axios
-          .put(
-            `https://crudcrud.com/api/eee789f4d71f444bafc280a5366d4dd7/candiesDetails/${id}`,
-            obj
-          )
-          .then((res) => {
-             displayCandies(obj);
-            // let d = obj.Quantity
-            // li.children[3].innerText = `Quantity: ${d}`;
-            items.remove(li)
-          })
-          .catch((er) => console.log(er.message));
-      })
-      .catch((err) => console.log(err.message));
+    remainningCandy(e,1,0);
   }
   if (e.target.classList.contains("buy-2")) {
-
+    remainningCandy(e,2,1);
   }
   if (e.target.classList.contains("buy-3")) {
+    remainningCandy(e,3,2);
   }
 }
 
 
+// impliment the remaining items list 
+function remainningCandy (e,dec , limit){
+  let li = e.target.parentNode;
+  let index = Array.from(li.parentNode.children).indexOf(li);
+  console.log(index);
+  let id = ids[index];
+  axios
+    .get(
+      `https://crudcrud.com/api/33d831562fea40a297815c4234f2823a/candiesDetails/${id}`
+    )
+    .then((res) => {
+      console.log(res.data);
+      let obj = res.data;
+     
+      if (obj.Quantity > limit) {
+        obj.Quantity = obj.Quantity - dec;
+        if(obj.Quantity === 0){
+          items.removeChild(li);
+          ids.splice(index,1);
+          axios.delete(`https://crudcrud.com/api/33d831562fea40a297815c4234f2823a/candiesDetails/${id}`).then(()=>{
+        
+          })
+        }
+        console.log(obj.Quantity);
+        let obj1 = {
+          Candy: obj.Candy,
+          Flavor: obj.Flavor,
+          Price: obj.Price,
+          Quantity: obj.Quantity,
+        };
+        return obj1;
+      } 
+       else {
+        // what happen if i return the error and trow the error at here
+          
+          console.log(e.target)
+        throw new Error("Candies are finished");
+      }
+    
+    })
+    .then((obj) => {
+      if(obj.Quantity != 0){
 
+        axios
+        .put(
+          `https://crudcrud.com/api/33d831562fea40a297815c4234f2823a/candiesDetails/${id}`,
+          obj
+          )
+          .then(() => {
+            let quantityElement =
+            items.children[index].querySelector(".Quantity");
+            quantityElement.textContent = `Quantity : ${obj.Quantity}`;
+          });
+        }
+    })
+    .catch((err) => console.log(err.message));
 
+}
